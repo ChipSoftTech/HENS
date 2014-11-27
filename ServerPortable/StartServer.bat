821c2c5927b3a34c21b2c1b81e0670a37b044d6c
chipsofttech <briancarter@chipsofttech.com>
2014-11-27 11:19:59 -0500

    bug fix in serverportable\startserver.bat file, added hens.bat, removed examples

--- ServerPortable/StartServer.bat
+++ ServerPortable/StartServer.bat
@@ -1,9 +1,10 @@
 
-: @echo off
+:Echo turned off, for debugging comment out the below with :
+@echo off
 
 : store a reference to the server directory (the path of %0) and CD there
-set SERVER_DIR=%~p0
-cd /D SERVER_DIR
+set SERVER_DIR=%~dp0
+cd /D %SERVER_DIR%
 
 : add curl to the path
 :http://curl.haxx.se/gknw.net/7.34.0/dist-w32/curl-7.34.0-rtmp-ssh2-ssl-sspi-zlib-idn-static-bin-w32.zip
@@ -31,10 +32,11 @@ set NODEDIR=%SERVER_DIR%bin\node
 set PATH=%NODEDIR%;%PATH%
 set NODE_PATH=%NODEDIR%\node_modules\;%NODEDIR%\node_modules\npm\node_modules;%NODEDIR%\node_modules\npm;%NODEDIR%\node_modules\sqlite3;%NODEDIR%\node_modules\nodemon;
 
-:sqlite3
+:sqlite3 install if required
 :npm -g install sqlite3
 
 start cmd.exe /k "cd ..\code"
+exit
 
 
 
