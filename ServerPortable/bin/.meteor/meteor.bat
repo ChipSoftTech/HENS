4d09c73d5434d9891730a0f0dfce9d121ba91b28
chipsofttech <briancarter@chipsofttech.com>
2015-04-08 08:00:47 -0400

    Meteor update to 1.1.0.2

--- ServerPortable/bin/.meteor/meteor.bat
+++ ServerPortable/bin/.meteor/meteor.bat
@@ -1,4 +1,7 @@
 @echo off
-set METEOR_INSTALLATION=%~dp0%
-"%~dp0\packages\meteor-tool\1.1.0-winr.12\mt-os.windows.x86_32\meteor.bat" %*
-rem %~dp0\packages\meteor-tool\1.1.0-winr.12\mt-os.windows.x86_32\meteor.bat
+SETLOCAL
+SET METEOR_INSTALLATION=%~dp0%
+"%~dp0\packages\meteor-tool\1.1.3\mt-os.windows.x86_32\meteor.bat" %*
+ENDLOCAL
+EXIT /b %ERRORLEVEL%
+rem %~dp0\packages\meteor-tool\1.1.3\mt-os.windows.x86_32\meteor.bat
